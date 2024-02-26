b63f02fc1f7fbfc6f9ab804b51aba5fb1ab2dbba
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-02-26 14:59:32 +0300

    Build Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  StarsCanvas,
+  Tech,
+  Works,
+} from './components/index';
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
