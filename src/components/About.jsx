0858d37413deba473f5a5de6bd294b4ff8b1044d
Ibrahim Tekfidan <ibrahim.tekfidn@gmail.com>
2024-02-27 09:26:58 +0300

    Build hoc SectionWrapper

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,16 +5,21 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import SectionWrapper from '../hoc/SectionWrapper';
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
-        variants={(fadeIn('right'), 'spring', 0.5 * index, 0.75)}
+        variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
-          options={{ max: 45, scale: 1, speed: 450 }}
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt="title" className="w-16 h-16 object-contain " />
@@ -53,4 +58,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, 'about');
